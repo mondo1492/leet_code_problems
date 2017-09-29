cbbd2e7fb80a864f276cd7707dd7d5f6482b7daa
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-09-29 14:33:52 -0700

    Solve two binary tree problems

--- leetcode_problems.rb
+++ leetcode_problems.rb
@@ -16,17 +16,70 @@ prices = [7, 6, 4, 3, 1]
 p max_profit(prices)
 
 def max_profit(prices)
-    return 0 if prices.length <= 1
-    current_low = prices[0]
-    current_high = prices[0]
-    profit = 0
-    prices[1..-1].each do |price|
-        if price >= current_high
-            current_high = price
-        else
-            profit += current_high - current_low
-            current_low, current_high = price, price
-        end
+  return 0 if prices.length <= 1
+  current_low = prices[0]
+  current_high = prices[0]
+  profit = 0
+  prices[1..-1].each do |price|
+    if price >= current_high
+      current_high = price
+    else
+      profit += current_high - current_low
+      current_low, current_high = price, price
     end
-    profit + current_high - current_low
+  end
+  profit + current_high - current_low
+end
+
+#Problem 623 ADD ONE ROW TO TREE
+def add_node(current_node, v)
+    left = current_node.left if current_node.left
+    right = current_node.right if current_node.right
+    current_node.left = TreeNode.new(v)
+    current_node.right = TreeNode.new(v)
+    current_node.left.left = left
+    current_node.right.right = right
+end
+
+def add_one_row(root, v, d)
+    queue = [[root, 1]]
+
+    if d == 1
+        new_root = TreeNode.new(v)
+        new_root.left = root
+        return new_root
+    end
+
+    until queue.empty?
+        current_node, current_depth = queue.shift
+
+        if current_depth == d - 1
+            add_node(current_node, v)
+        end
+
+        queue.push([current_node.left, current_depth + 1]) if current_node.left
+        queue.push([current_node.right, current_depth + 1]) if current_node.right
+    end
+    root
+end
+
+#PROBLEM 199 BINARY TREE RIGHT SIDE VIEW
+def right_side_view(root)
+    return [] unless root
+    queue = [[root, 0]]
+    results_arr = []
+
+    until queue.empty?
+        current_node, current_depth = queue.shift
+        queue.push([current_node.left, current_depth + 1]) if current_node.left
+        queue.push([current_node.right, current_depth + 1]) if current_node.right
+
+        # if queue.empty?
+        #     results_arr.push(current_node.val)
+        # elsif queue[0][1] != current_depth
+        #     results_arr.push(current_node.val)
+        # end
+        results_arr[current_depth] = current_node.val
+    end
+    results_arr
 end
