9939724b0e5762c582b1e80905741f763d1ff205
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-09-29 16:11:19 -0700

    Solve phone letter combinations

--- leetcode_problems.rb
+++ leetcode_problems.rb
@@ -83,3 +83,32 @@ def right_side_view(root)
     end
     results_arr
 end
+
+def letter_combinations(digits)
+    arr = []
+    nums =
+    {
+        "2" => ['a', 'b', 'c'],
+        "3" => ['d', 'e', 'f'],
+        "4" => ['g', 'h', 'i'],
+        "5" => ['j', 'k', 'l'],
+        "6" => ['m', 'n', 'o'],
+        "7" => ['p', 'q', 'r', 's'],
+        "8" => ['t', 'u', 'v'],
+        "9" => ['w', 'x', 'y', 'z']
+        }
+    digits.to_s.chars.each do |digit|
+        if arr.empty?
+            arr = nums[digit]
+        else
+            new_arr = []
+            arr.each do |let|
+                nums[digit].each do |let2|
+                    new_arr.push(let+let2)
+                end
+            end
+            arr = new_arr
+        end
+    end
+    arr
+end
