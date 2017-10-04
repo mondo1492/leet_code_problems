41d5045c0522b72ecd2514f34bf51ffd556cd3fd
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-10-04 13:18:25 -0700

    sovle path number

--- leetcode_problems_2.rb
+++ leetcode_problems_2.rb
@@ -1,6 +1,9 @@
+#15
 #Given an array S of n integers, are there elements a, b, c in S such
 #that a + b + c = 0? Find all unique triplets in the array which gives
 #the sum of zero.
+
+#figure this one out later
 
 def three_sum(nums)
   hash = Hash.new(0)
@@ -40,4 +43,49 @@ end
   # arr
 
 
-p three_sum([-1, 0, 1, 2, -1, -4])
+#p three_sum([-1, 0, 1, 2, -1, -4])
+
+#A robot is located at the top-left corner of a m x n grid
+#(marked 'Start' in the diagram below).
+
+#The robot can only move either down or right at any point in time.
+#The robot is trying to reach the bottom-right corner of the grid
+#(marked 'Finish' in the diagram below).
+
+#How many possible unique paths are there?
+
+# def unique_paths(m, n)
+#   col = []
+#   row = []
+#   m.times do |num1|
+#
+#   end
+# end
+
+def go_down(current_pos)
+  new_x = current_pos[0]
+  new_y = current_pos[1] + 1
+  [new_x, new_y]
+end
+
+def go_right(current_pos)
+  new_x = current_pos[0] + 1
+  new_y = current_pos[1]
+  [new_x, new_y]
+end
+
+def unique_paths(m, n)
+  #create matrix
+  #add up the up and left cells
+  matrix = Array.new(m) { Array.new(n, 1)}
+
+  matrix[1..-1].each_with_index do |x, i1|
+    matrix[1..-1].each_with_index do |y, i2|
+      matrix[i1 + 1][i2 + 1] = matrix[i1][i2 + 1] + matrix[i1 + 1][i2]
+    end
+  end
+  matrix[-1][-1]
+
+end
+
+p unique_paths(4, 4)
