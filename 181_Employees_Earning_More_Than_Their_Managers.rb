76b54da5eb018ff657545ae1ba1040821726bf0d
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-10-05 12:45:15 -0700

    fix formatting

--- 181_Employees_Earning_More_Than_Their_Managers.rb
+++ 181_Employees_Earning_More_Than_Their_Managers.rb
@@ -1,9 +1,10 @@
 <<-SQL
   SELECT
-    *
+    a.Name AS 'Employee'
   FROM
     Employee AS a,
     Employee AS b
   WHERE
-    a.ManagerId = b.Id AND a.Salary > b.Salary
+    a.ManagerId = b.Id AND
+    a.Salary > b.Salary
 SQL
